e16750d7ceb70edcb1d3897c76224dc2f518657a
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-31 14:30:36 -0300

    commit selectonSort

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const livros = require('./listaLivros')
 //                    0   1   2   3   4   5  este é o valor de arrays
 
-
+function menorValor(arrayProdutos, posicaoInicial){
 // entao o maisBarato comecara pela posicao 0 de cima pois ambos equivalem a 0
 let maisBarato = 0
-
-for (let atual = 0; atual < livros.length; atual ++){
-    if(livros[atual].preco< livros[maisBarato].preco){
+for (let atual = 0; atual < arrayProdutos.length; atual ++){
+    if(arrayProdutos[atual].preco< arrayProdutos[maisBarato].preco){
         maisBarato = atual
-    }   
-    
+        }     
+    }
+    return maisBarato
 }
-console.log(`O livro mais barato custa ${livros[maisBarato].preco} e seu titulo é ${livros[maisBarato].titulo}`)
+module.exports = menorValor
---
+++ selectionSort.js
@@ -0,0 +1,9 @@
+const livros = require('./listaLivros')
+const menorValor = require('./index')
+
+
+
+// Para a let atual de valor 0, sempre haverá loop enquanto atual for menor do que o tamanho(length) de livros. No final temos o incremento que andará de 1 em 1.
+    for(let atual = 0; atual < livros.length; atual++){
+
+    }
