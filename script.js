8e4bed5aa9c139417700c0ef99e9946c4caba2f2
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-31 14:09:02 -0300

    commit
    module.exports e import entre arquivos

---
+++ script.js
@@ -0,0 +1,14 @@
+// For
+
+//contador infinito
+for(let   i = 1  ;  i >= 0  ;  i++   ){
+    console.log('estou aprendendo!', i)
+}
+
+
+
+// for(let   i = 0  ;  i <= 10  ;  i++   ){
+//     if(i % 2 !== 0){
+//         console.log(i)
+//     }
+// }
